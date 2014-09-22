1c703a7b8cd96a1dbbe1086453443888fc57873c
Eddie McWhirter <emcwhirter@usgs.gov>
2014-09-21 20:33:52 -0600

    Create php shells for login and logout.

---
+++ src/htdocs/login.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * - login.php?returnto=URL&username&password
+ *     Have user?      --> Y -->  Redirect
+ *        ↓ N                        ↑ Y
+ *     Authenticating  --> Y -->  Valid User?
+ *                  ↓             ↓ N
+ *                  ↓ N       Show error
+ *                  ↓           ↓
+ *                  Show login form
+ */
+
+?>
---
+++ src/htdocs/logout.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * - Clear session
+ * - Redirect
+ */
+
+?>
